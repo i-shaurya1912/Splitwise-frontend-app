4595b2a0c21ff78c27e4eaa05fecbee88853baa4
i-shaurya1912 <vshaurya1912@gmail.com>
2026-08-22 17:10:41 +0530

    add .env file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL 
-  ? `${import.meta.env.VITE_API_URL}/api`
-  : 'http://localhost:5000/api';
+const rawBase = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_BASE_URL = `${rawBase.replace(/\/$/, '')}/api`;
 
 const API = axios.create({
   baseURL: API_BASE_URL,
